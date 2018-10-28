9c5e1fe150b78311b47f4c02b3cc417b6fded5ac
kafka <a50541853@gmail.com>
2018-10-28 21:37:35 +0800

    第二对多路复用器与显像管组合完成

--- five_char_display/five_char_dis.vhd
+++ five_char_display/five_char_dis.vhd
@@ -4,7 +4,11 @@ USE ieee.std_logic_1164.ALL;
 ENTITY five_char_dis IS
 PORT(
   SW:IN STD_LOGIC_VECTOR(17 DOWNTO 0);
-  HEX0:OUT STD_LOGIC_VECTOR(0 TO 6)
+  HEX0:OUT STD_LOGIC_VECTOR(0 TO 6);
+  HEX1:OUT STD_LOGIC_VECTOR(0 TO 6);
+  HEX2:OUT STD_LOGIC_VECTOR(0 TO 6);
+  HEX3:OUT STD_LOGIC_VECTOR(0 TO 6);
+  HEX4:OUT STD_LOGIC_VECTOR(0 TO 6)
 );
 END five_char_dis;
 
@@ -24,11 +28,16 @@ ARCHITECTURE five_char_dis_arch OF five_char_dis IS
   END COMPONENT;
 
 SIGNAL M : STD_LOGIC_VECTOR(2 DOWNTO 0);
+SIGNAL M_S_one : STD_LOGIC_VECTOR(2 DOWNTO 0);
 
 BEGIN
   M0: five_to_one_mult_comp PORT MAP (SW(17 DOWNTO 15), SW(14 DOWNTO  12), SW(11 DOWNTO 9), SW(8 DOWNTO 6), SW(5 DOWNTO 3), SW(2 DOWNTO 0), M);--signal M
   --SW has use but not declared ?  ..bcause the arch's blong is wrong .....
+  M1: five_to_one_mult_comp PORT MAP (SW(17 DOWNTO 15), SW(2 DOWNTO 0),SW(14 DOWNTO  12), SW(11 DOWNTO 9), SW(8 DOWNTO 6), SW(5 DOWNTO 3),  M_S_one);
+
   H0: seven_seg_comp PORT MAP (M, HEX0);--signal M选择
+
+  H1: seven_seg_comp PORT MAP (M_S_one, HEX1);
 END five_char_dis_arch;
 
 
