1a26af1253aae5812a0099e72330537046c83805
kafka <a50541853@gmail.com>
2018-10-29 10:30:07 +0800

    五字母显示器补充led连接

--- five_char_display/five_char_dis.vhd
+++ five_char_display/five_char_dis.vhd
@@ -8,7 +8,9 @@ PORT(
   HEX1:OUT STD_LOGIC_VECTOR(0 TO 6);
   HEX2:OUT STD_LOGIC_VECTOR(0 TO 6);
   HEX3:OUT STD_LOGIC_VECTOR(0 TO 6);
-  HEX4:OUT STD_LOGIC_VECTOR(0 TO 6)
+  HEX4:OUT STD_LOGIC_VECTOR(0 TO 6);
+  LEDR:OUT STD_LOGIC_VECTOR(17 DOWNTO 0);
+  LEDG:OUT STD_LOGIC_VECTOR(17 DOWNTO 0)
 );
 END five_char_dis;
 
@@ -34,15 +36,15 @@ SIGNAL M_S_three : STD_LOGIC_VECTOR(2 DOWNTO 0);
 SIGNAL M_S_four : STD_LOGIC_VECTOR(2 DOWNTO 0);
 
 BEGIN
-  M0: five_to_one_mult_comp PORT MAP (SW(17 DOWNTO 15), SW(14 DOWNTO  12), SW(11 DOWNTO 9), SW(8 DOWNTO 6), SW(5 DOWNTO 3), SW(2 DOWNTO 0), M);--signal M
-  --SW has use but not declared ?  ..bcause the arch's blong is wrong .....
-  M1: five_to_one_mult_comp PORT MAP (SW(17 DOWNTO 15), SW(2 DOWNTO 0),SW(14 DOWNTO  12), SW(11 DOWNTO 9), SW(8 DOWNTO 6), SW(5 DOWNTO 3),  M_S_one);
+  M0: five_to_one_mult_comp PORT MAP (SW(17 DOWNTO 15), SW(14 DOWNTO  12), SW(11 DOWNTO 9), SW(8 DOWNTO 6), SW(5 DOWNTO 3), SW(2 DOWNTO 0), M);--H first
 
-  M2: five_to_one_mult_comp PORT MAP (SW(17 DOWNTO 15),  SW(5 DOWNTO 3),SW(2 DOWNTO 0),SW(14 DOWNTO  12), SW(11 DOWNTO 9), SW(8 DOWNTO 6),  M_S_two);
+  M1: five_to_one_mult_comp PORT MAP (SW(17 DOWNTO 15), SW(2 DOWNTO 0),SW(14 DOWNTO  12), SW(11 DOWNTO 9), SW(8 DOWNTO 6), SW(5 DOWNTO 3),  M_S_one);--E first
 
-  M3: five_to_one_mult_comp PORT MAP (SW(17 DOWNTO 15), SW(8 DOWNTO 6),  SW(5 DOWNTO 3),SW(2 DOWNTO 0),SW(14 DOWNTO  12), SW(11 DOWNTO 9),  M_S_three);
+  M2: five_to_one_mult_comp PORT MAP (SW(17 DOWNTO 15),  SW(5 DOWNTO 3),SW(2 DOWNTO 0),SW(14 DOWNTO  12), SW(11 DOWNTO 9), SW(8 DOWNTO 6),  M_S_two);-- L first
 
-  M4: five_to_one_mult_comp PORT MAP (SW(17 DOWNTO 15),SW(11 DOWNTO 9), SW(8 DOWNTO 6),  SW(5 DOWNTO 3),SW(2 DOWNTO 0),SW(14 DOWNTO  12),  M_S_four);
+  M3: five_to_one_mult_comp PORT MAP (SW(17 DOWNTO 15), SW(8 DOWNTO 6),  SW(5 DOWNTO 3),SW(2 DOWNTO 0),SW(14 DOWNTO  12), SW(11 DOWNTO 9),  M_S_three);-- L2 frst 
+
+  M4: five_to_one_mult_comp PORT MAP (SW(17 DOWNTO 15),SW(11 DOWNTO 9), SW(8 DOWNTO 6),  SW(5 DOWNTO 3),SW(2 DOWNTO 0),SW(14 DOWNTO  12),  M_S_four);-- O first
 
   H0: seven_seg_comp PORT MAP (M, HEX0);--signal M选择
   H1: seven_seg_comp PORT MAP (M_S_one, HEX1);
@@ -50,7 +52,12 @@ BEGIN
   H3: seven_seg_comp PORT MAP (M_S_three, HEX3);
   H4: seven_seg_comp PORT MAP (M_S_four, HEX4);
 
-
+  LEDR<=SW;
+  LEDG(2 DOWNTO 0)<=M;
+  LEDG(5 DOWNTO 3)<=M_S_one;
+  LEDG(8 DOWNTO 6)<=M_S_two;
+  LEDG(11 DOWNTO 9)<=M_S_three;
+  LEDG(14 DOWNTO 12)<=M_S_four;
 END five_char_dis_arch;
 
 
